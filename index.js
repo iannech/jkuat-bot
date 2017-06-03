e08fe0af19c2b3e4f9d205dc950e807d64946243
iannech <ianoniango94@gmail.com>
2017-06-03 20:39:15 +0300

    parsing in html

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ tbot.on('message', (msg) => {
         return;
       }
       var output = res.responses.join(' ');
-      tbot.sendMessage(fromId, output);
+      tbot.sendMessage(parse_mode = 'HTML',fromId, output);
       if (!clientName) {
         db.set('userid:' + fromId, res.client_name, redis.print);
       }
